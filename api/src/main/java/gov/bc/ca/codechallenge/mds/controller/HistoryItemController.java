5cfa84001a7384639a4784f06e7f54f352fc2107
isuru-aot <isuru.gunawardana@aot-technologies.com>
2022-12-16 02:54:23 +0530

    rework the history items

--- api/src/main/java/gov/bc/ca/codechallenge/mds/controller/HistoryItemController.java
+++ api/src/main/java/gov/bc/ca/codechallenge/mds/controller/HistoryItemController.java
@@ -8,9 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,10 +33,6 @@ public class HistoryItemController {
 		return resp;
 	}
 
-	@GetMapping("/history-item/{id}")
-	public HistoryItem getHistoryItemById(@PathVariable(value = "id") Long historyItemId) {
-		return null;
-	}
 
 	@PostMapping("/history-item")
 	public HistoryItem createHistoryItem(@Valid @RequestBody HistoryItem historyItem) {
@@ -53,10 +47,4 @@ public class HistoryItemController {
 
 	}
 
-	@PutMapping("/history-item/{id}")
-	public HistoryItem updateHistoryItem(@PathVariable(value = "id") Long historyItemId,
-			@Valid @RequestBody HistoryItem historyItem) {
-		return null;
-
-	}
 }
