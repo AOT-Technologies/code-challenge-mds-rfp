dd648655ce18fb3f1329b327cf769d53292a526c
Jaise Thomas <jaise.thomas@aot-technologies.com>
2022-12-15 12:08:19 -0800

    spring props for hibernate

---
+++ api/src/main/java/gov/bc/ca/codechallenge/mds/JpaAuditingConfiguration.java
@@ -0,0 +1,8 @@
+package gov.bc.ca.codechallenge.mds;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {}
