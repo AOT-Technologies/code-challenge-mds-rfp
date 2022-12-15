283c783f713294d3562877f73e3beae79e29d657
mdyyyyy <mike.wise@aot-technologies.com>
2022-12-15 14:19:27 -0800

    Change database create to use hyphen instead of underscore

--- deployment/sql/01-create_database.sh
+++ deployment/sql/01-create_database.sh
@@ -1,1 +1,1 @@
-psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'mds_review'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE mds_review"
+psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'mds-review'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE mds-review"
