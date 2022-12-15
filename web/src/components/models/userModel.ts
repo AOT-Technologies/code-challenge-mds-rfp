e08694975466b64d597fc671ddd46ab18491b795
Henry Oforeh <henry.oforeh@aot-technologies.com>
2022-12-15 13:08:07 -0700

    remove user model

--- web/src/components/models/userModel.ts
+++
@@ -1,9 +0,0 @@
-export interface UserModel {
-  id?: number;
-  firstName: string;
-  middleName: string;
-  lastName: string;
-  dateOfBirth: Date;
-  createdDateTime?: Date;
-  updatedDateTime?: Date;
-}
